66812cc62caf7cf333f75278d71a348d66d5e8ed
Mike Klemarewski <mike.klemarewski@gmail.com>
2023-03-02 19:25:17 -0800

    Install oh my zsh and remove some unnecessary packages

--- install.sh
+++ install.sh
@@ -22,3 +22,26 @@ brew bundle
 
 # find the installers and run them iteratively
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+FILE=~/.zshrc.pre-oh-my-zsh
+if test -f "$FILE"; then
+    rm ~/.zshrc.pre-oh-my-zsh
+    rm ~/.zshrc
+    ln -s "$(pwd)/zsh/zshrc.symlink" ~/.zshrc
+fi
+
+## Install powerline fonts
+echo "› installing powerline fonts"
+git clone https://github.com/powerline/fonts.git
+pushd fonts
+./install.sh
+popd
+
+rm -rf ./fonts
+
+echo "Set your iTerm font to a powerline font for the zsh theme to work." 
+
+
